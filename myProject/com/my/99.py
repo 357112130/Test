af36159297e4f99b54ccefeb181e31c09fcf0a4a
357112130 <aptx_48691>
2019-01-18 21:26:47 +0800

    更新代码

---
+++ myProject/com/my/99.py
@@ -0,0 +1,10 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+# @author: ycg
+# @description: 
+# @date: create in 2019/1/4 22:02
+# @file：99.py
+# @modified By:
+for i in range(1,9):
+	for j in range(1,i):
+		print (j + "*" + i + "=" + (j*i))
---
+++ myProject/com/my/weather.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+# @author: ycg
+# @description: 
+# @date: create in 2018/12/29 20:50
+# @file：weather.py
+# @modified By:
+
