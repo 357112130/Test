ac4ad513b4dcde7d0b6449dfbefeaff1a8e85a21
357112130 <aptx_48691>
2019-03-02 19:28:03 +0800

    optimization

--- logintest/com/testcase/logintest/test02.py
+++ logintest/com/testcase/logintest/test02.py
@@ -7,7 +7,7 @@
 # @modified By:
 import unittest
 from selenium import webdriver
-import time
+from time import sleep
 
 
 class Test(unittest.TestCase):
@@ -26,7 +26,7 @@ class Test(unittest.TestCase):
 		print("结束单个测试用例")
 
 	# 定义登录方法
-	def login(self, sername, password):
+	def login(self, username, password):
 		self.driver = webdriver.Chrome()
 		self.driver.get("https://mail.qq.com/")
 		self.driver.maximize_window()
@@ -34,7 +34,7 @@ class Test(unittest.TestCase):
 		login_ifarme = self.driver.find_element_by_id("login_frame")
 		self.driver.switch_to.frame(login_ifarme)
 		self.driver.find_element_by_id("").clear()
-		self.driver.find_element_by_id("").send_keys(sername)
+		self.driver.find_element_by_id("").send_keys(username)
 		self.driver.find_element_by_id("p").clear()
 		self.driver.find_element_by_id("p").send_keys(password)
 		self.driver.find_element_by_id("login_btton").click()
@@ -50,7 +50,7 @@ class Test(unittest.TestCase):
 	def test_login_sername_error(self):
 		# 用户名错误,密码正确
 		self.login("错误的用户名", "aptx_48691")
-		time.sleep(10)
+		sleep(10)
 		error_message = self.driver.find_element_by_id("error_tips").text
 		# 断言错误的用户名tips=error_message
 		self.assertIn("请输入正确的帐号！", error_message)
@@ -60,7 +60,7 @@ class Test(unittest.TestCase):
 	def test_login_password_error(self):
 		# 用户名正确,密码错误
 		self.login("357112130@qq.com", "aptx_486")
-		time.sleep(10)
+		sleep(10)
 		error_message = self.driver.find_element_by_id("error_tips").text
 		# 断言错误的密码tips=error_message
 		self.assertIn("你输入的帐号或密码不正确，请重新输入。", error_message)
@@ -70,7 +70,7 @@ class Test(unittest.TestCase):
 	def test_login_sername_nll(self):
 		# 用户名为空,密码正确
 		self.login("", "aptx_48691")
-		time.sleep(10)
+		sleep(10)
 		error_message = self.driver.find_element_by_id("error_tips").text
 		# 断言用户名为空tips=error_message
 		self.assertEqal(error_message, "你还没有输入帐号！")
@@ -80,7 +80,7 @@ class Test(unittest.TestCase):
 	def test_login_password_nll(self):
 		# 用户名正确,密码为空
 		self.login("357112130@qq.com", "")
-		time.sleep(10)
+		sleep(10)
 		error_message = self.driver.find_element_by_id("error_tips").text
 		# 断言密码为空tips=error_message
 		self.assertEqal(error_message, "你还没有输入密码！")
