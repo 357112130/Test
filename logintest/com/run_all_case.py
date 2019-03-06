06de5de6495a358dfb21d91d5060be11753732ec
357112130 <aptx_48691>
2019-03-06 22:03:54 +0800

    调试优化

--- logintest/com/run_all_case.py
+++ logintest/com/run_all_case.py
@@ -30,7 +30,7 @@ def allcase():
 if __name__ == "__main__":
 	# runner=unittest.TextTestRunner()
 	# runner.run(allcase())
-	report_path = "D://pyProject//logintest//com//testcase//report_test//result_logintest.html"
+	report_path = "D://pyProject//logintest//com//testcase//report//result_logintest.html"
 	fp = open(report_path, "wb")
 	# runner = BeautifulReport(allcase())
 	# runner.report_test(description=u"test报告",filename=u"自动化测试unittest测试框架报告")
